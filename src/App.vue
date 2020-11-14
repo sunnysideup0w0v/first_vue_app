03a5e7ffbb20dc4a994d234039b1f5da4e5cc6bd
sunnysideup0w0v <723973972538@naver.com>
2020-11-14 13:37:59 +0900

    Add: TodoList Layout 구성중

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoList,
+  },
+};
 </script>
 
 <style>
@@ -21,7 +20,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
+  display: flex;
+  justify-content: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <h1>To do List</h1>
+    <input @input="inputChange" id="todoInput" placeholder="Please enter your to-dos" />
+    <ul>
+      <li>될까?</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  methods: {
+    inputChange(e) {
+      console.log(e.target.value);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+h1 {
+  text-align: center;
+  font-weight: 600;
+  font-size: 48px;
+}
+input {
+  width: 100%;
+  padding: 10px;
+}
+.wrapper {
+  width: 960px;
+}
+</style>
